dd7121a75dde3d22274ea4389876f8138b00e9eb
Christian König <koenig.kersting@gmail.com>
2017-10-27 16:58:46 +0200

    project initialized

---
+++ slider_task/views.py
@@ -0,0 +1,14 @@
+from otree.api import Currency as c, currency_range
+from . import models
+from ._builtin import Page, WaitPage
+from .models import Constants
+
+
+class Sliders(Page):
+    pass
+
+
+
+page_sequence = [
+    Sliders
+]
